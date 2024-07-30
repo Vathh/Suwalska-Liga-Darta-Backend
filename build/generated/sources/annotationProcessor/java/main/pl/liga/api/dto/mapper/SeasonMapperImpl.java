0e41e5282eca32ea904c8662cd3808acfef7c986
Maciej Przestrzelski <przestrzelski.maciej1@gmail.com>
2024-07-30 20:11:40 +0200

    cleaning files

--- build/generated/sources/annotationProcessor/java/main/pl/liga/api/dto/mapper/SeasonMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/pl/liga/api/dto/mapper/SeasonMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-07-29T16:15:18+0200",
+    date = "2024-07-30T20:10:17+0200",
     comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.4.jar, environment: Java 18.0.2 (Oracle Corporation)"
 )
 @Component
--- build/generated/sources/annotationProcessor/java/main/pl/liga/infrastructure/database/repository/mapper/ResultEntityMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/pl/liga/infrastructure/database/repository/mapper/ResultEntityMapperImpl.java
@@ -22,7 +22,7 @@ import pl.liga.infrastructure.database.entity.TournamentEntity;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-07-29T16:15:17+0200",
+    date = "2024-07-30T20:10:17+0200",
     comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.4.jar, environment: Java 18.0.2 (Oracle Corporation)"
 )
 @Component
